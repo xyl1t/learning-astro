eb4555813a298fc5bfd9544f01858fd4958ee13c
Xylit <56483075+xyl1t@users.noreply.github.com>
2025-05-01 21:49:47 +0200

    add preact and dark mode button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://marats-first-astro-site.netlify.app/"
+  site: "https://marats-first-astro-site.netlify.app/",
+  integrations: [preact()]
 });
