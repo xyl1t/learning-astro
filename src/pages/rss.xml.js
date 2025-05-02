14e4ba3069740caca21df25b62bf8b23803f8477
Xylit <56483075+xyl1t@users.noreply.github.com>
2025-05-02 21:10:39 +0200

    convert blog to collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,20 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
 
 export async function GET(context) {
+  const posts = await getCollection("blog");
+
   return rss({
     title: 'Marats Astro Learner | Blog',
     description: 'My (Marats) journey learning Astro',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
